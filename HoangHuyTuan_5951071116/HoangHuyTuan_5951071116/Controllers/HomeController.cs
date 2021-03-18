b8c918f87df2c635d6615f2c771da614a1205555
= <=>
2021-03-19 00:27:22 +0700

    tuan

---
+++ HoangHuyTuan_5951071116/HoangHuyTuan_5951071116/Controllers/HomeController.cs
@@ -0,0 +1,108 @@
+﻿using HoangHuyTuan_5951071116.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoangHuyTuan_5951071116.Controllers
+{
+    public class HomeController : Controller
+    {
+        db dbop = new db();
+        string msg;
+        public IActionResult Index()
+        {
+            Employee emp = new Employee();
+            emp.flag = "get";
+            DataSet ds = dbop.Empget(emp, out msg);
+            List<Employee> list = new List<Employee>();
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                list.Add(new Employee
+                {
+                    Sr_no = Convert.ToInt32(dr["Sr_no"]),
+                    Emp_name = dr["Emp_name"].ToString(),
+                    City = dr["City"].ToString(),
+                    State = dr["State"].ToString(),
+                    Country = dr["Country"].ToString(),
+                    Department = dr["Department"].ToString()
+                });
+            }
+            return View(list);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create([Bind] Employee emp)
+        {
+            try
+            {
+                emp.flag = "insert";
+                dbop.Empdml(emp, out msg);
+                TempData["msg"] = msg;
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = ex.Message;
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Edit(int id)
+        {
+            Employee emp = new Employee();
+            emp.Sr_no = id;
+            emp.flag = "getid";
+            DataSet ds = dbop.Empget(emp, out msg);
+            foreach (DataRow dr in ds.Tables[0].Rows)
+            {
+                emp.Sr_no = Convert.ToInt32(dr["Sr_no"]);
+                emp.Emp_name = dr["Emp_name"].ToString();
+                emp.City = dr["City"].ToString();
+                emp.State = dr["State"].ToString();
+                emp.Country = dr["Country"].ToString();
+                emp.Department = dr["Department"].ToString();
+            }
+            return View(emp);
+        }
+        [HttpPost]
+        public IActionResult Edit(int id, [Bind] Employee emp)
+        {
+            try
+            {
+                emp.Sr_no = id;
+                emp.flag = "update";
+                dbop.Empdml(emp, out msg);
+                TempData["msg"] = msg;
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = ex.Message;
+            }
+            return RedirectToAction("Index");
+        }
+        [HttpGet]
+        public IActionResult Delete(int id, [Bind] Employee emp)
+        {
+            try
+            {
+                emp.Sr_no = id;
+                emp.flag = "update";
+                dbop.Empdml(emp, out msg);
+                TempData["msg"] = msg;
+            }
+            catch (Exception ex)
+            {
+                TempData["msg"] = ex.Message;
+            }
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ HoangHuyTuan_5951071116/HoangHuyTuan_5951071116/Models/db.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HoangHuyTuan_5951071116.Models
+{
+    public class db
+    {
+        SqlConnection con = new SqlConnection(@"Data Source=DESKTOP-KR3IR33;Initial Catalog=DemoCRUD;Integrated Security=True");
+        public DataSet Empget(Employee emp, out string msg)
+        {
+            DataSet ds = new DataSet();
+            msg = "";
+            try
+            {
+                SqlCommand com = new SqlCommand("Sp_Employee", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("Sr_no", emp.Sr_no);
+                com.Parameters.AddWithValue("Emp_name", emp.Emp_name);
+                com.Parameters.AddWithValue("City", emp.City);
+                com.Parameters.AddWithValue("STATE", emp.State);
+                com.Parameters.AddWithValue("Country", emp.Country);
+                com.Parameters.AddWithValue("Department", emp.Department);
+                com.Parameters.AddWithValue("flag", emp.flag);
+                SqlDataAdapter da = new SqlDataAdapter(com);
+                da.Fill(ds);
+                msg = "OK";
+                return ds;
+            }
+            catch (Exception ex)
+            {
+                msg = ex.Message;
+                return ds;
+            }
+        }
+        public string Empdml(Employee emp, out string msg)
+        {
+            msg = "";
+            try
+            {
+                SqlCommand com = new SqlCommand("Sp_Employee", con);
+                com.CommandType = CommandType.StoredProcedure;
+                com.Parameters.AddWithValue("Sr_no", emp.Sr_no);
+                com.Parameters.AddWithValue("Emp_name", emp.Emp_name);
+                com.Parameters.AddWithValue("City", emp.City);
+                com.Parameters.AddWithValue("STATE", emp.State);
+                com.Parameters.AddWithValue("Country", emp.Country);
+                com.Parameters.AddWithValue("Department", emp.Department);
+                com.Parameters.AddWithValue("flag", emp.flag);
+                con.Open();
+                com.ExecuteNonQuery();
+                con.Close();
+                msg = "OK";
+                return msg;
+            }
+            catch (Exception ex)
+            {
+                if (con.State == ConnectionState.Open)
+                {
+                    con.Close();
+                }
+                msg = ex.Message;
+                return msg;
+            }
+        }
+    }
+}
